a70443bc4d9b466adf97a7012ae06c2f9383983a
Kjetil  Overby <kjeoever@msn.com>
2021-12-08 21:44:58 +0100

    selector

--- pages/oversikt/toolinputedit.js
+++ pages/oversikt/toolinputedit.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ToolInputCard from "../../src/components/oversikt/ToolInputCard";
 import { MyContext } from "../../src/contexts/MyContext";
 import HeaderStartPage from "../../src/components/common/HeaderStartPage";
@@ -6,27 +6,84 @@ import HeaderStartPage from "../../src/components/common/HeaderStartPage";
 const Toolinputedit = () => {
   const { toolwasteData, toolCreateData } = useContext(MyContext);
 
+  const [type, setType] = useState();
+  const [optionValue, setOptionValue] = useState("a");
+
+  useEffect(() => {
+    if (toolwasteData) {
+      setType(toolwasteData.filter((item) => item.type === optionValue));
+    }
+  }, [toolwasteData, optionValue]);
+
+  const handleChange = (e) => {
+    setOptionValue(e.target.value);
+  };
+  console.log(type);
   return (
     <>
       <HeaderStartPage />
       <div className="container">
         <div>
           <h1>Vraket</h1>
-          {toolwasteData &&
-            toolwasteData.map((item) => {
-              return (
-                <ToolInputCard
-                  type={item.type}
-                  antall={item.antall}
-                  inputText="Antall slettet"
-                  input={item.input}
-                  date={item.date}
-                  img={item.img}
-                  color="linear-gradient(to top, #fff1f1 0%, #f8e2e2 100%)"
-                  key={item._id}
-                />
-              );
-            })}
+          <select name="tools" id="tools" onChange={handleChange}>
+            <option value="a">Alle</option>
+            <option value="Reduserer segment høyre">
+              Reduserer segment høyre
+            </option>
+            <option value="Reduserer segment venstre">
+              Reduserer segment venstre
+            </option>
+            <option value="Fres 1/4 segment høyre">
+              Fres 1/4 segment høyre
+            </option>
+            <option value="Fres 1/4 segment venstre">
+              Fres 1/4 segment venstre
+            </option>
+            <option value="Fres 2/3 segment høyre">
+              Fres 2/3 segment høyre
+            </option>
+            <option value="Fres 2/3 segment venstre">
+              Fres 2/3 segment venstre
+            </option>
+            <option value="Fres 3 segment høyre">Fres 3 segment høyre</option>
+            <option value="Fres 3 segment venstre">
+              Fres 3 segment venstre
+            </option>
+            <option value="Fres 3 segment venstre">
+              Fres 3 segment venstre
+            </option>
+          </select>
+          {optionValue !== "a"
+            ? type.map((item) => {
+                return (
+                  <ToolInputCard
+                    type={item.type}
+                    antall={item.antall}
+                    inputText="Antall slettet"
+                    input={item.input}
+                    date={item.date}
+                    img={item.img}
+                    color="linear-gradient(to top, #fff1f1 0%, #f8e2e2 100%)"
+                    key={item._id}
+                  />
+                );
+              })
+            : optionValue === "a" &&
+              toolwasteData &&
+              toolwasteData.map((item) => {
+                return (
+                  <ToolInputCard
+                    type={item.type}
+                    antall={item.antall}
+                    inputText="Antall slettet"
+                    input={item.input}
+                    date={item.date}
+                    img={item.img}
+                    color="linear-gradient(to top, #fff1f1 0%, #f8e2e2 100%)"
+                    key={item._id}
+                  />
+                );
+              })}
         </div>
 
         <div>
